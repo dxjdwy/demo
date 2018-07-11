d12a33894d88315646a21a6d736dd8b87d929b1f
dxjdwy <dxjdwy@163.com>
2018-07-11 20:20:52 +0800

    test

---
+++ demo/src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index(Model model){
+        return "index";
+    }
+    @RequestMapping("/contact")
+    public String contact(Model model){
+        return "contact";
+    }
+    @RequestMapping("/about")
+    public String about(Model model){
+        return "about";
+    }
+    @RequestMapping("/1")
+    public String one(Model model){
+        return "1";
+    }
+}
